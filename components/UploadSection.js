107f8b0c99a6f38c2a4014b1a261bb9c4e1a7b23
Tausur <legendhits33@gmail.com>
2026-01-03 23:13:07 +0600

    Pdf upload features done

--- components/UploadSection.js
+++ components/UploadSection.js
@@ -39,7 +39,7 @@ export default function UploadSection() {
   };
 
   return (
-    <section className="mt-5 py-10 flex items-center justify-center bg-gradient-to-br from-purple-600 via-indigo-500 to-cyan-400 rounded-lg">
+    <section className="mt-5 py-10 flex items-center justify-center bg-linear-to-br from-purple-600 via-indigo-500 to-cyan-400 rounded-lg">
       <div className="bg-white/90 backdrop-blur-md rounded-2xl shadow-2xl p-10 md:w-[60%] w-[80%] text-center">
 
         <h1 className="text-3xl font-bold text-gray-900">
